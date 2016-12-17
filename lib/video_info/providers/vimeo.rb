06993d42bb209af313356cabdf8eabf26e7c10c9
Cory Becker <cory@beckerwebsolutions.com>
2016-12-17 14:21:46 -0600

    Revise regex

--- lib/video_info/providers/vimeo.rb
+++ lib/video_info/providers/vimeo.rb
@@ -15,7 +15,7 @@ class VideoInfo
       end
 
       def self.usable?(url)
-        url =~ %r{([vimeo|vimeopro]\.com\/(?!album|hubnut\/album).*)}
+        url =~ %r{((?:vimeo|vimeopro)\.com\/(?!album|hubnut\/album).*)}
       end
 
       def provider
